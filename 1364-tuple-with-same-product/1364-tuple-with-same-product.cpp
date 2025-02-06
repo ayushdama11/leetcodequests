7cc91e3b2e55f4dbd8cad25381e66d0728239cb0
Ayush Dama <114308646+ayushdama11@users.noreply.github.com>
2025-02-06 14:04:56 +0530

    Time: 298 ms (82.44%), Space: 84.9 MB (67.71%) - LeetHub

--- 1364-tuple-with-same-product/1364-tuple-with-same-product.cpp
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int nC2(int n) {
+    int fact(int n) {
         return (n*(n-1)/2);     // n pairs mese 2 ka group nikalna hai islie 
     }
     int tupleSameProduct(vector<int>& nums) {
         int n = nums.size();
+        int count = 0;
         unordered_map<int, int> mp;
         for(int i=0; i<n-1; i++) {
             for(int j=i+1; j<n; j++) {
                 mp[nums[i]*nums[j]]++;
             }
         }
-        int count = 0;
         for(auto e : mp) {
-            count += 8 * nC2(e.second);
+            count += 8 * fact(e.second);
         }
 
         return count;
